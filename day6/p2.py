3e752f9c9a3267d16952a7432ab9155c53574003
Massimo Albano <67909039+massimoa15@users.noreply.github.com>
2022-12-06 15:50:06 -0500

    Day 6 Completed

---
+++ day6/p2.py
@@ -0,0 +1,27 @@
+def IsListUnique(list):
+    for x in range(len(list)):
+        for y in list[x+1:]:
+            if list[x] == y: return False
+    return True
+
+# Read input
+#file = open('example.txt','r')
+file = open('input.txt','r')
+
+# Get the input buffer
+buffer = file.readline()
+
+# Our current position offset from the start of the list
+offset = 0
+
+while True:
+    # Get 4 char buffer
+    temp = buffer[offset:offset+14]
+
+    # Check if unique. If yes, we're done
+    if IsListUnique(temp): break
+
+    # Not unique, move forward
+    offset += 1
+
+print(offset+14)
